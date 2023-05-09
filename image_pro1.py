327144667ab1df8bc57d4b7df081ff1130916244
BishaliSaha01 <132124750+BishaliSaha01@users.noreply.github.com>
2023-05-09 09:31:45 +0530

    image_pro1.py

--- image_pro1.py
+++ image_pro1.py
@@ -1,7 +1,8 @@
 #Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
 #Type "help", "copyright", "credits" or "license()" for more information.
-import cv2
+#import cv2
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 import streamlit as st
 from PIL import Image
