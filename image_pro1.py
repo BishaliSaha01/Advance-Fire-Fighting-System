f2afc7053bf5e8716ec5f418d677925e461bdc6e
BishaliSaha01 <132124750+BishaliSaha01@users.noreply.github.com>
2023-05-10 19:34:15 +0530

    image_pro1.py

--- image_pro1.py
+++ image_pro1.py
@@ -30,8 +30,7 @@ def color_assigning(sensor_data,axis):
 #st.set_page_config(page_title="Building's website", page_icon=":tada:", layout="wide") 
     
 cred = credentials.Certificate('firebase-sdk.json')
-firebase_admin.initialize_app(cred,{'sec':{
-    'databaseURL': 'https://advance-fire-fighting-system-default-rtdb.asia-southeast1.firebasedatabase.app'}})
+firebase_admin.initialize_app(cred,name = 'sec)
 ref = db.reference('/')
 ref.set(
   {
