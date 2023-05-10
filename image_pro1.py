ce19fddbd3cae82195ca1386d65e492173087d0c
BishaliSaha01 <132124750+BishaliSaha01@users.noreply.github.com>
2023-05-10 20:17:24 +0530

    image_pro1.py

--- image_pro1.py
+++ image_pro1.py
@@ -16,11 +16,11 @@ def color_assigning(sensor_data,axis):
     #input_img = "image_processing.jpg"
     i1_img = cv.imread('image_processing.jpg')
     if sensor_data<15:
-        cv.circle(i1_img,(axis[0],axis[1]),100,(0,0,150),-1)
+       i1_img= cv.circle(i1_img,(axis[0],axis[1]),100,(0,0,150),-1)
     elif sensor_data>15 and sensor_data<40:
-        cv.circle(i1_img,(axis[0],axis[1]),100,(0,20,180),-1)
+       i1_img = cv.circle(i1_img,(axis[0],axis[1]),100,(0,20,180),-1)
     else:
-        cv.circle(i1_img,(axis[0],axis[1]),100,(1,100,255),-1)
+       i1_img = cv.circle(i1_img,(axis[0],axis[1]),100,(1,100,255),-1)
    # cv2.imshow("Drawing Shapes", i1_img)
     #cv2.waitKey(0)
     #var1 = Image.open("image_processing.jpg")
