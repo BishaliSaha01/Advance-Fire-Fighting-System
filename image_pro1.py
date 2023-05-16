f3f2f8ba4cc07d0440f545c52596439bd2333542
BishaliSaha01 <132124750+BishaliSaha01@users.noreply.github.com>
2023-05-16 09:04:08 +0530

    image_pro1.py

--- image_pro1.py
+++ image_pro1.py
@@ -73,15 +73,15 @@ for i in range(0,len(i1)):
     color_assigning(sensor_data,d1.get(input1,-1))
     fire_call(sensor_data)
     
-
+st.set_page_config(page_title="Building's website", page_icon=":tada:", layout="wide",reload=True) 
 while True:
-    st.set_page_config(page_title="Building's website", page_icon=":tada:", layout="wide") 
+    
     var1 = Image.open("image_processing.jpg") 
     st.image(var1)
     time.sleep(5)
     refresh_placeholder.empty()
         
-#st.set_page_config(page_title="Building's website", page_icon=":tada:", layout="wide")  
+#st.set_page_config(page_title="Building's website", page_icon=":tada:", layout="wide", reload=True)  
 #var1 = Image.open("image_processing.jpg") 
 #st.image(var1)
 
